505084703e104bc90835f273b17a03fd2a9469eb
H. Türkü Kaya <htkaya90@gmail.com>
2015-09-02 18:56:38 +0300

    Otomatik Arama

---
+++ OtomatikArama/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace OtomatikArama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.callPanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.makeCallButton = new System.Windows.Forms.Button();
+            this.txtReceivers = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.announcementCombo = new System.Windows.Forms.ComboBox();
+            this.didCombo = new System.Windows.Forms.ComboBox();
+            this.callPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(218, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // callPanel
+            // 
+            this.callPanel.Controls.Add(this.didCombo);
+            this.callPanel.Controls.Add(this.announcementCombo);
+            this.callPanel.Controls.Add(this.label4);
+            this.callPanel.Controls.Add(this.label3);
+            this.callPanel.Controls.Add(this.label2);
+            this.callPanel.Controls.Add(this.label1);
+            this.callPanel.Controls.Add(this.makeCallButton);
+            this.callPanel.Controls.Add(this.txtReceivers);
+            this.callPanel.Controls.Add(this.txtTitle);
+            this.callPanel.Location = new System.Drawing.Point(77, 41);
+            this.callPanel.Name = "callPanel";
+            this.callPanel.Size = new System.Drawing.Size(347, 205);
+            this.callPanel.TabIndex = 1;
+            this.callPanel.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Başlık";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Çalınacak Ses Kaydı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Aranacak Numaralar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Arayan Numara";
+            // 
+            // makeCallButton
+            // 
+            this.makeCallButton.Location = new System.Drawing.Point(118, 171);
+            this.makeCallButton.Name = "makeCallButton";
+            this.makeCallButton.Size = new System.Drawing.Size(75, 23);
+            this.makeCallButton.TabIndex = 4;
+            this.makeCallButton.Text = "Arama Yap";
+            this.makeCallButton.UseVisualStyleBackColor = true;
+            this.makeCallButton.Click += new System.EventHandler(this.makeCallButton_Click);
+            // 
+            // txtReceivers
+            // 
+            this.txtReceivers.Location = new System.Drawing.Point(141, 97);
+            this.txtReceivers.Name = "txtReceivers";
+            this.txtReceivers.Size = new System.Drawing.Size(160, 20);
+            this.txtReceivers.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(141, 17);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(160, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // announcementCombo
+            // 
+            this.announcementCombo.FormattingEnabled = true;
+            this.announcementCombo.Location = new System.Drawing.Point(141, 132);
+            this.announcementCombo.Name = "announcementCombo";
+            this.announcementCombo.Size = new System.Drawing.Size(160, 21);
+            this.announcementCombo.TabIndex = 9;
+            // 
+            // didCombo
+            // 
+            this.didCombo.FormattingEnabled = true;
+            this.didCombo.Location = new System.Drawing.Point(141, 57);
+            this.didCombo.Name = "didCombo";
+            this.didCombo.Size = new System.Drawing.Size(160, 21);
+            this.didCombo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 302);
+            this.Controls.Add(this.callPanel);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.callPanel.ResumeLayout(false);
+            this.callPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel callPanel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button makeCallButton;
+        private System.Windows.Forms.TextBox txtReceivers;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ComboBox announcementCombo;
+        private System.Windows.Forms.ComboBox didCombo;
+    }
+}
+
---
+++ OtomatikArama/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Add Bulutfon refences
+using Bulutfon.OAuth;
+using Bulutfon.OAuth.Win;
+using Bulutfon.Sdk;
+using Bulutfon.Sdk.Models;
+using Bulutfon.Sdk.Models.Post;
+
+namespace OtomatikArama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string client_id = "d68a8d69c16b6ac209980dc5ec7b381933d91c71ca37d83e8e5c64b0ae2f3f9e";
+            string client_secret = "6b9f79ac744ce39a61b1ba236782b7de4d54a96f9f6c43077449cd86c9e9f799";
+
+            /* Tokenları Saklayıp her seferinde giriş yapmadan kullanabilirsiniz */
+
+            if (tokensPresentInDb())
+            {
+                /* Eğer tokenlar saklandıysa */
+                string access_token = getAccessTokenFromDb();
+                string refresh_token = getRefreshTokenFromDb();
+
+                Token tkn = new Token(access_token, refresh_token);
+                BulutfonWinClient client = new BulutfonWinClient(client_id, client_secret);
+                tkn.TokenExpired += client.tokenProviderTokenExpired;
+                Authentication.Token = tkn;
+                showCallPanel(true);
+            }
+            else
+            {
+                /* Baştan token al */
+                var loggedIn = LoginForm.Login(client_id, client_secret, this);
+                showCallPanel(loggedIn);
+            }
+            
+        }
+
+        private void showCallPanel(bool p)
+        {
+            if (p)
+            {
+                btnLogin.Visible = false;
+                callPanel.Visible = true;
+
+                // Expire olan token refreshlenince tetiklenecek method
+                Authentication.Token.RefreshCallback += TokensRefreshed;
+                List<Announcement> announcements = BulutfonApi.GetAnnouncements(Authentication.Token);
+                announcementCombo.DataSource = announcements;
+                announcementCombo.DisplayMember = "name";
+                announcementCombo.ValueMember = "id";
+
+                List<Did> dids = BulutfonApi.GetDids(Authentication.Token);
+
+                didCombo.DataSource = dids;
+                didCombo.DisplayMember = "number";
+                didCombo.ValueMember = "number";
+
+            }
+            else
+            {
+                MessageBox.Show("Bir Hata Oluştu!");
+            }
+        }
+
+        void TokensRefreshed(object sender, string access_token, string refreh_token)
+        {
+            MessageBox.Show("Tokenlar yenilendi.");
+            // Update Tokens on db
+        }
+
+        private bool tokensPresentInDb()
+        {
+            // Token Db de tutuluyor mu kontrolü
+            return false;
+        }
+
+        private string getAccessTokenFromDb()
+        {
+            return "ACCESS_TOKEN";
+        }
+
+        private string getRefreshTokenFromDb()
+        {
+            return "REFRESH_TOKEN";
+        }
+
+        private void makeCallButton_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text.ToString();
+            long did = Convert.ToInt64(didCombo.SelectedValue.ToString());
+            string receivers = txtReceivers.Text.ToString();
+            int announcement_id = Convert.ToInt32(announcementCombo.SelectedValue.ToString());
+
+            AutomaticCallCreator auto = new AutomaticCallCreator();
+            auto.title = title;
+            auto.did = did;
+            auto.receivers = receivers;
+            auto.announcement_id = announcement_id;
+            ResponseAutomaticCall response = BulutfonApi.CreateAutomaticCall(Authentication.Token, auto);
+            if (response.message != null)
+            {
+                txtTitle.Clear();
+                txtReceivers.Clear();
+                MessageBox.Show(response.message);
+            }
+        }
+    }
+}
